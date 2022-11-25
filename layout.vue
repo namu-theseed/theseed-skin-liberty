05458d89f6bf7ae19135942178c5c6c28339ff22
Tenpower <94548284+Tenpower@users.noreply.github.com>
2022-11-25 18:33:14 +0900

    Update layout.vue
    최근 바뀜을 최근 변경으로 변경

--- layout.vue
+++ layout.vue
@@ -6,7 +6,7 @@
                 <nuxt-link class="navbar-brand" to="/"></nuxt-link>
                 <ul class="nav navbar-nav">
                     <li class="nav-item">
-                        <nuxt-link class="nav-link" to="/RecentChanges"><span class="fa fa-refresh"></span><span class="hide-title">최근 바뀜</span></nuxt-link>
+                        <nuxt-link class="nav-link" to="/RecentChanges"><span class="fa fa-refresh"></span><span class="hide-title">최근 변경</span></nuxt-link>
                     </li>
                     <li class="nav-item">
                         <nuxt-link class="nav-link" to="/RecentDiscuss"><span class="fa fa-comments"></span><span class="hide-title">최근 토론</span></nuxt-link>
